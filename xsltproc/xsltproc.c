ada8c1c7942be2efdcba4fe3abaa58addf6b9782
Maximilian Hils <git@maximilianhils.com>
2016-06-27 15:26:23 -0700

    fix vs2015 build

--- xsltproc/xsltproc.c
+++ xsltproc/xsltproc.c
@@ -57,7 +57,6 @@
 #if defined(_MSC_VER) || defined(__MINGW32__)
 #include <winsock2.h>
 #define gettimeofday(p1,p2)
-#define snprintf _snprintf
 #endif /* _MS_VER */
 #else /* WIN32 */
 #if defined(HAVE_SYS_TIME_H)
